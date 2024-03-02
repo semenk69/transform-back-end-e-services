869aad13521d50117ec247b54b531a45d8cccc0f
semenk69 <152292047+semenk69@users.noreply.github.com>
2024-03-02 11:35:19 +0300

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,17 @@
+    const data = [];
+    for (let i = 0; i < 10; i++) {
+    }
+    const data = generateRandomData();
+function generateRandomData() {
+
+
+}
+        console.log("Random Number: " + item);
+}
+
+function main() {
+main();
+    data.forEach(item => {
+    return data;
+        data.push(Math.floor(Math.random() * 100) + 1);
+    });
